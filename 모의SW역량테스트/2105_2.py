c46c218f670d33bb47103e916dceea81ed8bb20a
tjdls111 <tjdls111@naver.com>
2021-10-14 14:06:35 +0900

    디저트카페_수정

--- 모의SW역량테스트/2105_2.py
+++ 모의SW역량테스트/2105_2.py
@@ -25,14 +25,12 @@ def dfs(i, j, direction_type, dessert_cnt):
 
         # 범위 벗어나면 X
         if 0 <= ni < N and 0 <= nj < N:
-            if cafe_visited[ni][nj] == False and dessert_visited[arr[ni][nj]] == False:  # 방문하지 않은 카페 & 먹지 않은 디저트
+            if dessert_visited[arr[ni][nj]] == False:  # 방문하지 않은 카페 & 먹지 않은 디저트
 
-                cafe_visited[ni][nj] = True
                 dessert_visited[arr[ni][nj]] = True
 
                 dfs(ni, nj, k, dessert_cnt + 1)
 
-                cafe_visited[ni][nj] = False
                 dessert_visited[arr[ni][nj]] = False
 
 
@@ -42,7 +40,6 @@ for tc in range(1, T + 1):
     N = int(input())
     arr = list(list(map(int, input().split())) for _ in range(N))
 
-    cafe_visited = [[False] * N for _ in range(N)]
     dessert_visited = [False] * 101
     ans = -1
 
@@ -51,12 +48,10 @@ for tc in range(1, T + 1):
     for i in range(N):
         for j in range(N):
             start = (i, j)
-            cafe_visited[i][j] = True
             dessert_visited[arr[i][j]] = True
 
             dfs(i, j, 0, 1)
 
-            cafe_visited[i][j] = False
             dessert_visited[arr[i][j]] = False
 
     print(f'#{tc} {ans}')
